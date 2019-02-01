e2ffb710a6246956c4fc3fb3a1b3c478c4349231
Mike <M.donati@hotmail.com>
2019-02-01 10:32:10 -0500

    newest with slider

--- Phramd/wwwroot/js/site.js
+++ Phramd/wwwroot/js/site.js
@@ -5,18 +5,38 @@ window.onclick = function (event) {
     }
 }
 
-$(function (){
+$(function () {
         $(".gmailCal").on("click", function () {
             $(".hiddenGmail").toggle()
+            $(".hiddenApple").toggle(this.hidden)
+            $(".hiddenMicro").toggle(this.hidden)
         })
         $(".appleCal").on("click", function () {
             $(".hiddenApple").toggle()
+            $(".hiddenMicro").toggle(this.hidden)
+            $(".hiddenGmail").toggle(this.hidden)
         })
         $(".microCal").on("click", function () {
             $(".hiddenMicro").toggle()
+            $(".hiddenApple").toggle(this.hidden)
+            $(".hiddenGmail").toggle(this.hidden)
         })
         $(".gPhoto").on("click", function () {
             $(".hiddenGPhoto").toggle()
         })
     }
 )
+
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 5000);
+}
